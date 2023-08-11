d9851e648d7e993d674070cf9b772a6978096375
Somnath Bera <somnathbera1919@gmail.com>
2023-08-11 12:24:15 +0530

    signup functionality added

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,43 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+    console.log(reqBody);
+
+    //check user already exists
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    //hash password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    //create user
+    const newUser = new User({ username, email, password: hashedPassword });
+
+    //saving user in the database
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+
+    //return success response
+    return NextResponse.json({
+      message: "User created successfully",
+      success: true,
+      savedUser,
+    });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,21 +1,57 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
+import axios from "axios";
+import { Toaster, toast } from "react-hot-toast";
 
 const page = () => {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: "",
   });
 
-  const onSignup = async () => {};
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  //for changing signup button deppending on the input fields
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      toast("Signing you Up!");
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup Success " + response.data);
+      toast.success("Signup Success!");
+
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup Failed" + error);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>SignUp</h1>
+      <div>
+        <Toaster />
+      </div>
+      <h1>{loading ? "Processing" : "Signup"}</h1>
       <hr />
       <label htmlFor="username">username</label>
       <input
@@ -31,7 +67,7 @@ const page = () => {
       <input
         className="text-black p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         id="email"
-        type="text"
+        type="email"
         value={user.email}
         //everything but username are kept unchanged
         onChange={(e) => setUser({ ...user, email: e.target.value })}
@@ -52,7 +88,7 @@ const page = () => {
         onClick={onSignup}
         className=" p-2  border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       >
-        Signup
+        {buttonDisabled ? "no signup" : "signup"}
       </button>
       <Link
         href="/login"
