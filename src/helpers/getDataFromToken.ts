f40e685a2642fbe158d92b2a8f459077fe8e101f
Somnath Bera <somnathbera1919@gmail.com>
2023-08-11 16:59:53 +0530

    added middleware for validation

---
+++ src/helpers/getDataFromToken.ts
@@ -0,0 +1,7 @@
+import { NextRequest } from "next/server";
+import { Jwt } from "jsonwebtoken";
+
+export const getDataFromToken = (request: NextRequest) => {
+  try {
+  } catch (error) {}
+};
---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
+//logic
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath = path === "/login" || path === "/signup";
+
+  const token = request.cookies.get("token")?.value || "";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/profile", request.nextUrl));
+  }
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+}
+
+//matching paths
+export const config = {
+  matcher: ["/", "/profile/:path*", "/login", "/signup"],
+};
