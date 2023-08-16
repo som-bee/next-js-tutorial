5c4d78f9d19ecd2c481f2046c0a9bc329686b8ae
Somnath Bera <somnathbera1919@gmail.com>
2023-08-16 12:40:44 +0530

    retrived user data from token and udated profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,9 +3,12 @@ import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function UserProfile({ params }: any) {
   const router = useRouter();
+  const [data, setData] = useState("nothing");
+
   const logout = async () => {
     try {
       await axios.get("/api/users/logout");
@@ -17,6 +20,13 @@ export default function UserProfile({ params }: any) {
       toast.error(error.message);
     }
   };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data.username);
+  };
+
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
@@ -26,7 +36,11 @@ export default function UserProfile({ params }: any) {
         <p className="text-4xl">
           Hello{" "}
           <span className="p-2 rounded bg-orange-500 text-black ml-2">
-            {params.id}
+            {data === "nothing" ? (
+              "There!"
+            ) : (
+              <Link href={`/profile/${data}`}>{data}</Link>
+            )}
           </span>
         </p>
         <br />
@@ -35,6 +49,13 @@ export default function UserProfile({ params }: any) {
           className="px-4 py-2 rounded  bg-blue-500 hover:bg-blue-700 text-white"
         >
           Logout
+        </button>
+        <br />
+        <button
+          onClick={getUserDetails}
+          className="px-4 py-2 rounded  bg-green-800 hover:bg-green-900 text-white"
+        >
+          Get User Details
         </button>
       </div>
     </>
--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -1,7 +1,15 @@
 import { NextRequest } from "next/server";
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const getDataFromToken = (request: NextRequest) => {
   try {
-  } catch (error) {}
+    const encodedToken = request.cookies.get("token")?.value || "";
+    const decodedToken: any = jwt.verify(
+      encodedToken,
+      process.env.TOKEN_SECRET!
+    );
+    return decodedToken.id;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
 };
